c5ac1cf4cd50f87232c25bb82b67b7f18c0b1e6e
Petr Tripolsky <tripolskypetr@gmail.com>
2023-06-04 01:39:57 +0500

    rename

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-console.log('you should use this started kit by running "npx create-react-app . --template=appwrite"')
+console.log('you should use this started kit by running "npx create-react-app . --template=firebase-crm"')
 module.exports = {}
